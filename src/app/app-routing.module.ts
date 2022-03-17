f64189c72da973af703fbd51ef1671770e28e412
Leonardo Mata <leonardo.mata@arpage.com>
2022-03-17 19:57:23 +0100

    Initial components creation + Angular material instalation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameArenaPage } from './pages/game-arena/game-arena.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GameArenaPage,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/game-arena/game-arena.page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './game-arena.page.html',
+    styleUrls: ['./game-arena.page.scss']
+})
+export class GameArenaPage {
+}
